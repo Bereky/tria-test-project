102cc35cc883fc4634b0a239deebc468db4cce3a
unknown <bereketlemma4@gmail.com>
2024-03-10 21:00:53 +0300

    update card container

--- frontend/components/department/CardContainer.tsx
+++ frontend/components/department/CardContainer.tsx
@@ -1,28 +1,23 @@
-import Link from "next/link";
-
-import { IoIosAddCircleOutline } from "react-icons/io";
 import Card from "./Card";
-import AddButton from "./Button/AddButton";
 
-const CardContainer = () => {
+type Department = {
+  id: string;
+  name: string;
+  description: string;
+  managingDepartmentId: string;
+  createdAt: string;
+};
+
+type DepartmentState = {
+  departments: Department[];
+};
+
+const CardContainer = ({ data }: any) => {
   return (
     <div className="w-full h-auto grid md:grid-cols-4 gap-5">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-
-      <button className="col-span-1 h-40 outline outline-sky-500 bg-sky-50 hover:bg-sky-100 rounded-sm flex flex-col items-center justify-center p-4 gap-1  shadow-lg hover:shadow-md duration-300">
-        <h1 className="text-xl uppercase font-bold">CT0</h1>
-        <h1 className="text-md uppercase font-normal">
-          Cheif Technology Officer
-        </h1>
-      </button>
-
-      <button className="col-span-1 h-40 outline outline-sky-500 bg-sky-50 hover:bg-sky-100 rounded-sm flex flex-col items-center justify-center p-4 gap-1   shadow-lg hover:shadow-md duration-300">
-        <h1 className="text-xl uppercase font-bold">CF0</h1>
-        <h1 className="text-md uppercase font-normal">Cheif Finance Officer</h1>
-      </button>
+      {data?.map((item: Department) => {
+        return <Card item={item} />;
+      })}
     </div>
   );
 };
