dbb66be9451ba4b25e9a22675d4fbb5bc0a78fb0
unknown <bereketlemma4@gmail.com>
2024-03-10 21:01:32 +0300

    update department card

--- frontend/components/department/Card.tsx
+++ frontend/components/department/Card.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
-const Card = () => {
+const Card = ({ item }: any) => {
   return (
     <Link
-      href={"/department/1"}
+      href={`/department/${item.id}`}
       className="col-span-1 h-40 outline outline-sky-500 bg-sky-50 hover:bg-sky-100 rounded-sm flex flex-col items-center justify-center p-4 gap-1 shadow-lg hover:shadow-md duration-300"
     >
-      <h1 className="text-xl uppercase font-bold">CE0</h1>
+      <h1 className="text-xl uppercase font-bold">{item.name}</h1>
       <h1 className="text-md uppercase font-normal text-center">
-        Cheif Excutive Officer
+        {item.description}
       </h1>
     </Link>
   );
