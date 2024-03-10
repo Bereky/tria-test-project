40bea3ae5477114f5932e6fe353eba0875efc8ac
unknown <bereketlemma4@gmail.com>
2024-03-11 02:08:18 +0300

    add customer components

---
+++ frontend/components/customer/CustomerListLoading.tsx
@@ -0,0 +1,11 @@
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+const CustomerListLoading = () => {
+  return (
+    <div className="w-full h-auto min-h-80 bg-sky-100 p-3 flex justify-start items-center flex-col overflow-x-auto table-container rounded-sm">
+      <div className="w-full h-auto flex justify-start items-center bg-gray-00"></div>
+    </div>
+  );
+};
+
+export default CustomerListLoading;
